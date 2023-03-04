150d1a9a2339c711ec417133ad52200d9605f0b3
Giancarlos Salas <me@giansalex.dev>
2023-03-04 16:22:25 -0500

    Load nfts by user

--- src/App/pages/account/index.tsx
+++ src/App/pages/account/index.tsx
@@ -17,18 +17,13 @@ import {
   LinkOverlay,
 } from "@chakra-ui/react";
 import {
-  formatPrice,
   NftInfo,
-  CW721,
-  Market,
-  NftInfoResponse,
-  publicIpfsUrl,
   useSdk,
-  CW721Instance,
 } from "../../services";
-import { config } from "../../../config";
 import { NftCard } from "../../components";
 import userLogo from "../../assets/user-default.svg";
+import { IrisNFTClient, Nft } from "../../services/client/iris_nft";
+import { IDCollection } from "../../proto/nft/nft";
 
 interface AccountParams {
   readonly user: string;
@@ -39,24 +34,26 @@ const maxItemsPerPage = 15;
 export const Account = () => {
   const { user } = useParams<AccountParams>();
 
-  const { client, address } = useSdk();
+  const { nftClient, address } = useSdk();
   const [nfts, setNfts] = useState<NftInfo[]>([]);
-  const [nftSale, setNftSale] = useState<NftInfo[]>([]);
 
-  const getNftsInfo = async (ids: string[], contract: CW721Instance) => {
-    const allNfts: Promise<NftInfoResponse>[] = [];
-    ids.forEach(tokenId => {
-      allNfts.push(contract.nftInfo(tokenId));
+  const getNftsInfo = async (ids: IDCollection[], client: IrisNFTClient) => {
+    const allNfts: Promise<Nft>[] = [];
+    ids.forEach(collection => {
+      collection.tokenIds.forEach((tokenId) => {
+        allNfts.push(client.getNFT(collection.denomId, tokenId));
+      });
     });
 
     const tokens = await Promise.all(allNfts);
-    return tokens.map((nft, idx) => {
+    return tokens.map((nft) => {
       return {
-        tokenId: ids[idx],
+        denom: nft.cid,
+        tokenId: nft.id,
         user: 'unknown',
         title: nft.name,
         price: 'Not listed',
-        image: publicIpfsUrl(nft.image),
+        image: nft.uri?.endsWith(".png") ? nft.uri : "",
         total: 1
       };
     });
@@ -64,31 +61,14 @@ export const Account = () => {
 
   useEffect(() => {
     (async () => {
-      if (!client || !user) return;
+      if (!nftClient || !user) return;
 
-      const contract = CW721(config.contract).use(client);
-      const result = await contract.tokens(user, undefined, maxItemsPerPage);
-      setNfts(await getNftsInfo(result.tokens, contract));
+      const result = await nftClient.getCollections(user, maxItemsPerPage);
+      const nfts = await getNftsInfo(result.owner!.idCollections, nftClient);
+      console.log(nfts);
+      setNfts(nfts);
     })();
-  }, [client, user]);
-
-  useEffect(() => {
-    (async () => {
-      if (!client || !user) return;
-
-      const contract = CW721(config.contract).use(client);
-      const marketcw = Market(config.marketContract).use(client);
-      const result = await marketcw.offersBySeller(user, undefined, maxItemsPerPage);
-      const tokens = await getNftsInfo(result.offers.map(o => o.token_id), contract);
-
-      const items = tokens.map((nft, idx) => {
-        const off = result.offers[idx];
-        nft.price = formatPrice(off.list_price);
-        return nft;
-      });
-      setNftSale(items);
-    })();
-  }, [client, user]);
+  }, [nftClient, user]);
 
   const getNftPath = (nftId: string) => `${address === user ? "/account" : ""}/token/${nftId}`;
 
@@ -120,33 +100,16 @@ export const Account = () => {
           <Tabs
             isManual
             isLazy
-            defaultIndex={1}
             colorScheme="cyan">
             <TabList>
-              <Tab>On Sale</Tab>
               <Tab>Owned</Tab>
             </TabList>
 
             <TabPanels>
               <TabPanel>
                 <SimpleGrid spacing={10} gridTemplateColumns={["repeat(1, minmax(0px, 1fr))", "repeat(3, minmax(0px, 1fr))", "repeat(5, minmax(0px, 1fr))"]}>
-                  {nftSale.map(nft => (
-                    <LinkBox as="picture" key={nft.tokenId}
-                      transition="transform 0.6s cubic-bezier(0.165, 0.84, 0.44, 1) 0s"
-                      _hover={{
-                        transform: "scale(1.05)"
-                      }}>
-                      <LinkOverlay as={ReactRouterLink} to={getNftPath(nft.tokenId)}>
-                        <NftCard nft={nft} />
-                      </LinkOverlay>
-                    </LinkBox>
-                  ))}
-                </SimpleGrid>
-              </TabPanel>
-              <TabPanel>
-                <SimpleGrid spacing={10} gridTemplateColumns={["repeat(1, minmax(0px, 1fr))", "repeat(3, minmax(0px, 1fr))", "repeat(5, minmax(0px, 1fr))"]}>
                   {nfts.map(nft => (
-                    <LinkBox as="picture" key={nft.tokenId}
+                    <LinkBox as="picture" key={nft.denom + ":" + nft.tokenId}
                       transition="transform 0.6s cubic-bezier(0.165, 0.84, 0.44, 1) 0s"
                       _hover={{
                         transform: "scale(1.05)"
