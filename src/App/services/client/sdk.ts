048b4e99408bc47cbde9245cdcf4364b13482d7f
Giancarlos Salas <me@giansalex.dev>
2023-03-07 11:43:14 -0500

    create custom sign client for uptick

--- src/App/services/client/sdk.ts
+++ src/App/services/client/sdk.ts
@@ -1,12 +1,16 @@
 import { CosmWasmClient } from "@cosmjs/cosmwasm-stargate";
 import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
-import { defaultRegistryTypes, GasPrice } from "@cosmjs/stargate";
+import { Account, defaultRegistryTypes, GasPrice } from "@cosmjs/stargate";
+import { SigningStargateClient } from "@cosmjs/stargate";
 import { GeneratedType, OfflineSigner, Registry } from "@cosmjs/proto-signing";
 import { MsgTransfer } from "./../../proto/nft_transfer/tx";
+import { Any } from "cosmjs-types/google/protobuf/any";
 
 import { AppConfig } from "../config/network";
 import { MsgIssueDenom, MsgMintNFT } from "../../proto/nft/tx";
 import { MsgExecuteContract } from "cosmjs-types/cosmwasm/wasm/v1/tx";
+import { EthAccount } from "../../proto/ethermint/types/account";
+import { EtherminSigningClient, SinginClient } from "./eth_sign";
 
 export type WalletLoader = (chainId: string, addressPrefix?: string) => Promise<OfflineSigner>;
 
@@ -28,6 +32,37 @@ export async function createClient(config: AppConfig, signer: OfflineSigner): Pr
     gasPrice: GasPrice.fromString(`${config.gasPrice}${config.token.coinMinimalDenom}`),
     registry: createDefaultRegistry(),
   });
+}
+
+export async function createStargateClient(config: AppConfig, signer: OfflineSigner): Promise<SinginClient> {
+  let accountParser;
+  if (config.coinType === 60) {
+    accountParser = (data: Any): Account => {
+      if (data.typeUrl !== "/ethermint.types.v1.EthAccount") {
+        throw new Error(`Unexpected account type: ${data.typeUrl}`);
+      }
+      const acc = EthAccount.decode(data.value).baseAccount!;
+      return {
+        address: acc.address,
+        pubkey: {
+          type: acc.pubKey!.typeUrl,
+          value: acc.pubKey?.value,
+        },
+        accountNumber: acc.accountNumber.toNumber(),
+        sequence: acc.sequence.toNumber(),
+      }
+    }
+  }
+  const options = {
+    prefix: config.addressPrefix,
+    gasPrice: GasPrice.fromString(`${config.gasPrice}${config.token.coinMinimalDenom}`),
+    registry: createDefaultRegistry(),
+    accountParser,
+  };
+
+  const client = await SigningStargateClient.connectWithSigner(config.rpcUrl, signer, options);
+
+  return config.coinType === 60 ? new EtherminSigningClient(client, signer, options): client;
 }
 
 export function createSimpleClient(config: AppConfig): Promise<CosmWasmClient> {
